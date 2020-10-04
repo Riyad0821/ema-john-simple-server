0317d1cbba4d1e35a9ac477ec7042915fbf2ba0d
Riyad0821 <mdriyad0821@gmail.com>
2020-10-04 16:41:21 +0600

    load data from the server

--- index.js
+++ index.js
@@ -15,14 +15,27 @@ const port = 5000
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qguwb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const products = client.db("emaJohnStore").collection("products");
+  const productsCollection = client.db("emaJohnStore").collection("products");
   app.post('/addProduct', (req, res) => {
-    const product = req.body;  
-    products.insertOne(product)
+    const products = req.body;  
+    productsCollection.insertMany(products)
     .then(result => {
-        console.log(result);
+        console.log(result.insertedCount);
+        res.send(result.insertedCount)
     })
   })
+  app.get('/products', (req, res) => {
+      productsCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+  app.get('/products/:key', (req, res) => {
+    productsCollection.find({key: req.params.key})
+    .toArray((err, documents) => {
+      res.send(documents[0]);
+    })
+})
   console.log('Database Connected');
 });
 
