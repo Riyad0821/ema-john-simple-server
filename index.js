98bc851838a278c7857d4d2708927cc9d95f27cd
Riyad0821 <mdriyad0821@gmail.com>
2020-12-06 19:38:12 +0600

    Okay

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productsCollection = client.db("emaJohnStore").collection("products");
+  const ordersCollection = client.db("emaJohnStore").collection("orders");
   app.post('/addProduct', (req, res) => {
     const products = req.body;  
     productsCollection.insertMany(products)
@@ -43,6 +44,14 @@ app.post('/productsByKeys', (req, res) => {
         res.send(documents);
     })
 })
+
+app.post('/addOrder', (req, res) => {
+    const order = req.body;  
+    ordersCollection.insertOne(order)
+    .then(result => {
+        res.send(result.insertedCount > 0)
+    })
+  })
   console.log('Database Connected');
 });
 
